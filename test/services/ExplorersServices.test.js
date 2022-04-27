5ff7af2c68774916355119b7e80548e86e72fe14
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-04-26 21:40:48 -0600

    Agregada pruebas de unidad con Jest

---
+++ test/services/ExplorersServices.test.js
@@ -0,0 +1,9 @@
+const ExplorersService = require("../../lib/services/ExplorersService")
+
+describe("Test de explorer para service", () =>{
+    test("Requerimiento 1: Calcular los explorers en mision", () =>{
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorersService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1)
+    })
+})

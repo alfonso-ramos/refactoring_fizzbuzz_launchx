14200ddf5b231ef417a975ad7e7afd961df367f7
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-04-30 18:52:06 -0600

    Pasadas las pruebas de unidad en FizzBuzzService

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -24,22 +24,22 @@ describe("Test de FizzbuzzService", () =>{
 });
 describe("Test de metodo applyValidationInNumber", ()=>{
     test("Requerimiento 1: Retornar el el trick, el mismo valor que score", ()=>{
-        const score = {score:1};
+        const score = 1;
         expect(FizzBuzzService.applyValidationInNumber(score)).toBe(1);
         //console.log(FizzBuzzService.applyValidationInNumber(explorer));
     });
     test("Requerimiento 2: Retornar el valor FIZZ en el trick", () =>{
-        const score = {score: 3};
+        const score = 3;
         expect(FizzBuzzService.applyValidationInNumber(score)).toMatch(/FIZZ/);
         //console.log(FizzBuzzService.applyValidationInNumber(explorer));
     });
     test("Requerimiento 3: Retornar el valor BUZZ en el score", () =>{
-        const score = {score: 5};
+        const score = 5;
         expect(FizzBuzzService.applyValidationInNumber(score)).toMatch(/BUZZ/);
         //console.log(FizzBuzzService.applyValidationInNumber(explorer));
     });
     test("Requerimiento 4: Retornar el valor FIZZBUZZ en el trick", () =>{
-        const score = {score: 15};
+        const score = 15;
         expect(FizzBuzzService.applyValidationInNumber(score)).toMatch(/FIZZBUZZ/);
         //console.log(FizzBuzzService.applyValidationInNumber(explorer));
     });
